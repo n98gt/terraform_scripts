e85b940151c1f967e3f0ceda3a2044b54c6553bd
Roman <n98gt56ti@gmail.com>
2022-01-05 21:07:22 +0500

    files changed: 4
    A  aws_s3-bucket_and_dynamodb_for_remote_tfstate/credentials.example
    A  aws_s3-bucket_and_dynamodb_for_remote_tfstate/main.tf
    A  aws_s3-bucket_and_dynamodb_for_remote_tfstate/overrides.auto.tfvars.example
    A  aws_s3-bucket_and_dynamodb_for_remote_tfstate/vars.tf

---
+++ aws_s3-bucket_and_dynamodb_for_remote_tfstate/vars.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-north-1" # Stockholm
+}
+
+variable "s3_bucket_name" {
+  type = string
+}
+
+variable "dynamodb_table_name" {
+  type = string
+}
